56236a704350a27aa4b5a58644d6a5783c3c1e94
xo3337 <61803830+xo3337@users.noreply.github.com>
2026-05-10 02:49:15 +0300

    Fix to_str method to include rx and rz attributes

--- lib/datasets/kitti/kitti_utils.py
+++ lib/datasets/kitti/kitti_utils.py
@@ -113,7 +113,7 @@ class Object3d(object):
     def to_str(self):
         print_str = '%s %.3f %.3f %.3f box2d: %s hwl: [%.3f %.3f %.3f] pos: %s ry: %.3f' \
                      % (self.cls_type, self.trucation, self.occlusion, self.alpha, self.box2d, self.h, self.w, self.l,
-                        self.pos, self.ry)
+                        self.pos, self.rx, self.ry, self.rz)
         return print_str
 
 
